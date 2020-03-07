7d1a53459635a188e894a30b68d5663b10082e85
刘宏伟 <1174607294@qq.com>
2020-03-07 17:11:51 +0800

    修改jq引用地址到oos

--- config/request.js
+++ config/request.js
@@ -3,8 +3,8 @@ import router from "../route/route.js"
 import Vue from "vue";
 
 var request = axios.create({
-    //baseURL: 'http://47.254.47.158:3001',
-    baseURL: 'http://localhost:3000',
+    baseURL: '/',
+    //baseURL: 'http://localhost:3000',
     //timeout:10000,
     headers: {
         "Content-Type": 'application/json'
