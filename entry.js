c2aefbe7782a968531aa9fd3bbd1df75f5ba7329
刘宏伟 <1174607294@qq.com>
2020-12-26 00:00:53 +0800

    修改后台管理请求的地址

--- entry.js
+++ entry.js
@@ -4,84 +4,87 @@ const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const webpack = require("webpack")
 
 module.exports = {
-  mode:"development",
-  entry: {
-      main:'./index.js'
+    mode: "development",
+    entry: {
+        main: './index.js'
     },
-  devServer:{
-      index:"index.html",
-      publicPath: "/",
+    devServer: {
+        index: "index.html",
+        publicPath: "/",
+        proxy: {
+            '/houtai': 'http://localhost:3000'
+        }
 
-  },
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
-    chunkFilename:"[hash].[name].js"
-  },
-  optimization: {
-    splitChunks: {
-      name:'common',
-      minSize:1,
-      minChunks:1,
-      maxAsyncRequests:10,
-      chunks:'async'
     },
-  },
-  resolve:{
-    alias:{
-      'houtai':path.resolve(__dirname,"houtai"),
-      'img': path.resolve(__dirname,"img")
-    }
-  },
-  module:{
-    rules:[
-      {
-        test: /\.vue$/,
-        use:["vue-loader"]
-      },
-      {
-        test:/\.css$/,
-        use:["vue-style-loader","css-loader"]
-      },
-      {
-        test:/\.js$/,
-        use:["babel-loader?cacheDirectory=true"]
-      },
-      {
-        test:/\.less$/,
-        use:["vue-style-loader","css-loader","less-loader"]
-      },
-      {
-        test: /\.(png|jpg|gif)$/,
-        use:[
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+        chunkFilename: "[hash].[name].js"
+    },
+    optimization: {
+        splitChunks: {
+            name: 'common',
+            minSize: 1,
+            minChunks: 1,
+            maxAsyncRequests: 10,
+            chunks: 'async'
+        },
+    },
+    resolve: {
+        alias: {
+            'houtai': path.resolve(__dirname, "houtai"),
+            'img': path.resolve(__dirname, "img")
+        }
+    },
+    module: {
+        rules: [
             {
-                loader:"url-loader",
-                options:{
-                  name:'[path][name].[ext]',
-                  limit: 8192
-                }
+                test: /\.vue$/,
+                use: ["vue-loader"]
+            },
+            {
+                test: /\.css$/,
+                use: ["vue-style-loader", "css-loader"]
+            },
+            {
+                test: /\.js$/,
+                use: ["babel-loader?cacheDirectory=true"]
+            },
+            {
+                test: /\.less$/,
+                use: ["vue-style-loader", "css-loader", "less-loader"]
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: "url-loader",
+                        options: {
+                            name: '[path][name].[ext]',
+                            limit: 8192
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2)$/,
+                loader: 'file-loader'
             }
-          ]
-      },
-      {
-        test: /\.(eot|svg|ttf|woff|woff2)$/,
-        loader: 'file-loader'
-      }
-    ],
-  },
-  plugins:[
-    new webpack.DefinePlugin({
-      'env': JSON.stringify("environment"),
-    }),
-    new VueLoaderPlugin(),
-    new HtmlWebpackPlugin({
-        filename: 'index.html',
-        template: 'index.html',
-        inject: true
-      }),
-      // new webpack.DllReferencePlugin({
-      //   context: __dirname,
-      //   manifest: require("./static/js/vendor-manifest.json"),
-      // })
-  ]
+        ],
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'env': JSON.stringify("environment"),
+        }),
+        new VueLoaderPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'index.html',
+            inject: true
+        }),
+        // new webpack.DllReferencePlugin({
+        //   context: __dirname,
+        //   manifest: require("./static/js/vendor-manifest.json"),
+        // })
+    ]
 }
