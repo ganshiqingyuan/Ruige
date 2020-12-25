232f03960d5f9bc0d5aae9a990eceec3a245bb29
刘宏伟 <1174607294@qq.com>
2020-12-25 23:52:50 +0800

    增加登录页

---
+++ houtai/components/admin/login.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="login_warp">
+    <el-form
+      class="login_form
+    "
+      label-width="100px"
+    >
+      <el-form-item label="用户名">
+        <el-input v-model="username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="password"></el-input>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login",
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+};
+</script>
+
+<style lang="less">
+.login_warp {
+  .login_from {
+    width: 400px;
+    margin: 300px auto;
+  }
+}
+</style>
--- route/route.js
+++ route/route.js
@@ -1,28 +1,32 @@
 import VueRouter from "vue-router"
 
 export default new VueRouter({
-      routes: [
-            {
-                  path: '/',
-                  redirect: '/houtai/productmanage'
-            },
-            {
-                  path: '/houtai',
-                  component: () => import("houtai/components/houtai.vue"),
-                  children: [
-                        {
-                              path: '/houtai/productmanage',
-                              component: () => import('houtai/components/productmanage/product_type_list.vue')
-                        },
-                        {
-                              path: '/houtai/recommendmanage',
-                              component: () => import('houtai/components/recommendmanage/recommend_manage_list.vue')
-                        },
-                        {
-                              path: '/houtai/newsmanage',
-                              component: () => import('houtai/components/news/news_list.vue')
-                        }
-                  ]
-            },
-      ]
+    routes: [
+        {
+            path: '/',
+            redirect: '/houtai/productmanage'
+        },
+        {
+            path: '/houtai',
+            component: () => import("houtai/components/houtai.vue"),
+            children: [
+                {
+                    path: '/houtai/productmanage',
+                    component: () => import('houtai/components/productmanage/product_type_list.vue')
+                },
+                {
+                    path: '/houtai/recommendmanage',
+                    component: () => import('houtai/components/recommendmanage/recommend_manage_list.vue')
+                },
+                {
+                    path: '/houtai/newsmanage',
+                    component: () => import('houtai/components/news/news_list.vue')
+                }
+            ]
+        },
+        {
+            path: '/login',
+            component: () => import("houtai/login/login.vue")
+        }
+    ]
 })
