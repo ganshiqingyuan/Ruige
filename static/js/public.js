067dc934ee4005ba65b3d94d629bcf419d9b1422
刘宏伟 <1174607294@qq.com>
2021-01-13 01:16:06 +0800

    优化界面，增加删除产品的提示页面

--- static/js/public.js
+++ static/js/public.js
@@ -1,37 +1,33 @@
-window.onload=function(){
-	//控制panel
-		var can=0;
-		var cannone=1;
-		$('.panel').hover(function(){
-			if(can){
-				cannone=0;
-			}
-		},function(){
-			cannone=1;
-			$(this).css("display","none")
-		})
-		
-		$(".h-header li.prod").hover(function(){
-			$('.panel').css("display","block");
-			$(".panel li").css("min-height",parseInt($(".panel li:first-child").css("height"))+10+"px");
-		},function(){
-			can=1;
-			setTimeout(function(){
-				can=0;
-				if(cannone){
-					$('.panel').css("display","none")
-				}
-			},300);
-		})
-		//控制第一个导航条
-		$(".spread").click(function(){
-			console.log("123");
-			if($('.h-header ul').css("display")=="none")
-			{$(".h-header ul").css("display","block")}
-			else
-			{
-			$(".h-header ul").css("display","none")
-			}
-		})
+//控制panel
+var can = 0;
+var cannone = 1;
+$('.panel').hover(function () {
+    if (can) {
+        cannone = 0;
+    }
+}, function () {
+    cannone = 1;
+    $(this).css("display", "none")
+})
+
+$(".h-header li.prod").hover(function () {
+    $('.panel').css("display", "block");
+    $(".panel li").css("min-height", parseInt($(".panel li:first-child").css("height")) + 10 + "px");
+}, function () {
+    can = 1;
+    setTimeout(function () {
+        can = 0;
+        if (cannone) {
+            $('.panel').css("display", "none")
+        }
+    }, 300);
+})
+//控制第一个导航条
+$(".spread").click(function () {
+    console.log("123");
+    if ($('.h-header ul').css("display") == "none") { $(".h-header ul").css("display", "block") }
+    else {
+        $(".h-header ul").css("display", "none")
+    }
+})
 		//控制面板高度
-}
